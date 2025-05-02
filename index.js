2bec2eb66b33c20deb52b26eef55cfe4c3e65707
Duy <Duyhoang1147@gmail.com>
2025-05-02 09:53:04 +0700

    add connect database

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const conectDB = require('./database/connectDB'); // Adjust the path as necessary
+
 const app = express();
 const port = 3000;
 
+// Connect to MongoDB
+conectDB()
 // Middleware for parsing JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
